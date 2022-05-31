359e1c86b99a8bf8b6622b650f412cbbfa8aba20
YassinZaart <zaart45@gmail.com>
2022-05-31 16:09:20 +0300

    change db models

--- models.py
+++ models.py
@@ -6,39 +6,38 @@ from variables import db
 class UserModel(db.Model):
     __tablename__ = 'users'
     email = db.Column(db.String(20), primary_key=True)
-    name = db.Column(db.String(30), nullable=False)
-    phone_number = db.Column(db.String(10), nullable=False)
-    city = db.Column(db.String(20), nullable=False)
-    street = db.Column(db.String(20), nullable=False)
-    password = db.Column(db.String(50), nullable=False)
-    is_admin = db.Column(db.Boolean, nullable=False)
-
-
-class DoneeModel(db.Model):
-    __tablename__ = 'donees'
-    id = db.Column(db.String(20), primary_key=True)
-    first_name = db.Column(db.String(15), nullable=False)
-    last_name = db.Column(db.String(15), nullable=False)
-    city = db.Column(db.String(20), nullable=False)
-    street = db.Column(db.String(20), nullable=False)
-    phone_number = db.Column(db.String(10), nullable=False)
+    name = db.Column(db.String(30), nullable=False, unique=True)
+    phone_number = db.Column(db.String(10), nullable=False, default="N/A")
+    address = db.Column(db.String(50), nullable=False, default="N/A")
+    password = db.Column(db.String(50), nullable=False, default="N/A")
+    description = db.Column(db.String(500), nullable=False, default="N/A")
+    is_verified = db.Column(db.Boolean, nullable=False, default=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
 
 
 class DonationModel(db.Model):
     __tablename__ = 'donations'
+    id = db.Column(db.Integer, primaryKey=True, autoincrement=True)
     user_name = db.Column(db.String(30), ForeignKey('users.name'), primary_key=True)
-    donee_id = db.Column(db.String(20), ForeignKey('donees.id'), primary_key=True)
-    date = db.Column(db.DateTime, default=func.now(), primary_key=True)
-    type = db.Column(db.String(30), nullable=False)
+    date = db.Column(db.DateTime, default=func.now())
+    description = db.Column(db.String(500), nullable=False)
     value = db.Column(db.Integer, nullable=False)
 
 
 class PostModel(db.Model):
     __tablename__ = 'posts'
-    charity_name = db.Column(db.String(30), nullable=False, primary_key=True)
-    user_name = db.Column(db.String(30), nullable=False)
+    id = db.Column(db.Integer, primaryKey=True, autoincrement=True)
+    charity_name = db.Column(db.String(30), ForeignKey('users.name'), nullable=False)
+    name = db.Column(db.String(30), nullable=False)
     address = db.Column(db.String(30), nullable=False)
     phoneNumber = db.Column(db.String(30), nullable=False)
     description = db.Column(db.String(1000), nullable=False)
-    time_created = db.Column(db.DateTime, default=func.now(), primary_key=True)
+    value = db.Column(db.Integer, nullable=False, default=0)
+    time_created = db.Column(db.DateTime, default=func.now())
 
+
+class PostContributionModel(db.Model):
+    __tablename__ = 'post_contributions'
+    charity_name = db.Column(db.String(30), ForeignKey('users.name'), nullable=False)
+    post_id = db.Column(db.Integer, ForeignKey('posts.id'), primaryKey=True, autoincrement=True)
+    value = db.Column(db.Integer,nullable=False, default=0)
